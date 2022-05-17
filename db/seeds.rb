c546c65b375c8162a538ad2bbcd70682ddc4deb0
mansakondo <mansakondo22@gmail.com>
2022-05-17 15:40:54 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+inventory = Inventory.create(name: "Sample inventory")
+
+6.times do |i|
+  location = Location.new
+
+  location.name    = "Warehouse #{i}"
+  location.address = Faker::Address.street_address
+  location.city    = Faker::Address.city
+  location.country = Faker::Address.country
+
+  inventory.locations << location
+end
+
+6.times do
+  item = Item.create(name: Faker::Commerce.unique.product_name, inventory: inventory)
+
+  6.times do
+    stock = Stock.create(item: item, quantity: rand(10), location: inventory.locations.to_a.sample)
+  end
+end
