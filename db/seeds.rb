f62847171789361091bbc2bcead6a6e71d70cd5a
mansakondo <mansakondo22@gmail.com>
2022-05-17 22:50:15 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ inventory = Inventory.create(name: "Sample inventory")
 6.times do |i|
   location = Location.new
 
-  location.name    = "Warehouse #{i}"
+  location.name    = "Warehouse #{i+1}"
   location.address = Faker::Address.street_address
   location.city    = Faker::Address.city
   location.country = Faker::Address.country
@@ -14,7 +14,7 @@ end
 6.times do
   item = Item.create(name: Faker::Commerce.unique.product_name, inventory: inventory)
 
-  6.times do
-    stock = Stock.create(item: item, quantity: rand(10), location: inventory.locations.to_a.sample)
+  inventory.locations.each do |location|
+    stock = Stock.create(item: item, quantity: rand(10), location: location)
   end
 end
