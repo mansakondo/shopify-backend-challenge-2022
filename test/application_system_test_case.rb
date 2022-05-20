a9272c7c53d981b0b782c8cef9d0e38be0551b7d
mansakondo <mansakondo22@gmail.com>
2022-05-20 23:18:58 +0200

    Use `cuprite` with Capybara

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,19 @@
 require "test_helper"
+require "capybara/cuprite"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :cuprite, screen_size: [1400, 1400],
+    options: {
+      headless: ENV["HEADLESS_BROWSER_MODE"].blank?,
+    }
+
+  def pause
+    sleep 2 if ENV["HEADLESS_BROWSER_MODE"].present?
+  end
+
+  setup do
+    2.times do
+      current_window.maximize
+    end
+  end
 end
